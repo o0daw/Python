816190e7640fcaa00219f0cfb9314e3c1e6e9974
o0daw <112194026+o0daw@users.noreply.github.com>
2023-09-07 13:00:28 +0300

    списки

---
+++ copy_test.py
@@ -0,0 +1,43 @@
+# foods = ['Pizza', 'sushi', 'icecream']
+# you_food = foods[:]
+# you_food [-1] = 'meat'
+# print(foods)
+# print(you_food)
+# //--------------------------кортежі---------------------------------------------
+# foods = ('Pizza', 'sushi', 'icecream')
+# foods [0] = 'spagetti'
+# print(foods) 
+# (x, y) = (6, 8)
+# print(x)
+# print(y)
+# x, y = y, x
+# print(x)
+# print(y)
+# //-----------------------------------------------------------------------
+# foods = ('Pizza', 'sushi', 'icecream','sup', 'rice')
+# for food in foods:
+#     print(food)
+# //------------------------ділення по итерації-----------------------------------------------
+# s = 'HEllo'
+# print(list(s))
+# p = list(s)
+# p[0] = "O"
+# print(p)
+# //------------------------ділення по словам-----------------------------------------------
+# text = "Lorem ipsum dolor sit amet"
+# list_text = text.split()
+# print(list_text)
+# list_text.reverse()
+# print(" ".join(list_text))
+# //-----------------------------------------------------------------------
+# type_words = input("Введіть послідовність чисел:").split(", ")
+# print(type_words)
+# numbers = []
+# for i in type_words:
+#     numbers.append(int(i))
+# print(tuple(numbers))
+# //-----------------------------------------------------------------------
+
+
+
+
