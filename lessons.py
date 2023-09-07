1be9346e5f2ac5d26576a2fef3510e57577c8925
o0daw <112194026+o0daw@users.noreply.github.com>
2023-09-07 09:40:21 +0300

    2

---
+++ lessons.py
@@ -0,0 +1,60 @@
+# favorite_food = ['Potato', 'Becon', 'Eggs', 'Fruit salat']
+# print(favorite_food [0])
+# print(favorite_food [2])
+# print(favorite_food [-1])
+
+# -----------------------------------------------
+
+# L = [3,6,7,4,-5,4,3,-1]
+
+# total = sum(L)
+
+# if total > 2 : 
+#     Quantiti_element = len(L)
+#     print('Сума елементів списку більше ніж 2. Кількість елементів:', Quantiti_element)
+
+# else:
+#     print("Сума елементів списку не більще 2.")
+# d = max(L) - min(L)
+# if d > 10 :
+#     print(sorted(L))
+# ---------------------------------------------???--
+
+# L = [3,'Hello',7,4,'Привіт',4,3,-1]
+# if 'Привіт' in L:
+#     print(L * 10)
+#-------------------------------------------------
+#--------------------ЗРІЗИ-----------------------------
+# L = [3,'Hello',7,4,'Привіт',4,3,-1]
+# print(L[:3]) # зріз з 0 индексу до 3
+# print(L[:])  # зруіз з 0 ин до кінця
+# print(L[::2]) # зріз з 0 до кінця але кожен другий елемент
+# print(L[::-1]) # копія списку задом наперед
+# print(L[-1:]) # зріз від останнього до кінця тобото останній 
+# print(L[:-1]) #все крім останнього 
+#-------------------------------------------------
+# names = ['alise','david','karolina']
+# for name in names:
+#     print("Hello, ", name.title())
+#-------------------------------------------------
+# Favorit_Pizzas = ['Margarita',"4 Chises", "Karbonara", 'Hanter']
+# for Pizza in Favorit_Pizzas:
+#     print("I Like ", Pizza, "pizza")
+# print("I realy love pizza!")
+#-------------------------------------------------
+# print(list(range(0, 10 , 1)))
+# for i in range(1, 10, 2):
+#     print(i)
+#-------------------------------------------------
+    
+# squares = []
+# for i in range(1, 6):
+#     value = i ** 2
+#     squares.append(value)
+# print(squares)    
+#-------------------------------------------------
+# squares = [i**2 for i in range(1, 6)]
+# print(squares)
+#-------------------------------------------------
+4.4 4.7 4.8 4.9
+
